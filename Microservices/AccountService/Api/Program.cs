98abd653316b36fe10d7677a3cdd8f05afcbcaf4
andreshts <andres91411@gmail.com>
2026-05-18 16:32:46 -0600

    feat(k8s): configure GRPC and HTTP ports for AccountService and add AccountService endpoint to WalletService deployment
    Configured Kestrel to support both HTTP1 and HTTP2 for specified ports in AccountService. Added `GrpcServices__AccountService` environment variable to WalletService deployment for communication with AccountService.

--- Microservices/AccountService/Api/Program.cs
+++ Microservices/AccountService/Api/Program.cs
@@ -8,6 +8,17 @@ using Ecosystem.Infra.IoC.MultiTenancy;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var httpPort = int.Parse(builder.Configuration["ASPNETCORE_HTTP_PORTS"] ?? "8080");
+var grpcPort = int.Parse(builder.Configuration["ASPNETCORE_GRPC_PORT"] ?? "50051");
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(httpPort, listenOptions =>
+        listenOptions.Protocols = Microsoft.AspNetCore.Server.Kestrel.Core.HttpProtocols.Http1AndHttp2);
+    options.ListenAnyIP(grpcPort, listenOptions =>
+        listenOptions.Protocols = Microsoft.AspNetCore.Server.Kestrel.Core.HttpProtocols.Http2);
+});
+
 // Core services
 builder.Services.AddControllers()
     .AddNewtonsoftJson();
